0ee3ced1fdfd40006d4f61c3d3b15b1379c29afa
houjuunue <i@nue.im>
2022-12-31 16:01:22 +0800

    finish proxies page

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use crossterm::{
 		LeaveAlternateScreen,
 	},
 };
+use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
 use reqwest::blocking::Client;
 use serde::{Deserialize, Serialize};
 use std::{
@@ -23,73 +24,6 @@ use tui::{
 	widgets::{Block, Borders, List, ListItem, Tabs},
 	Frame, Terminal,
 };
-
-struct App {
-	http: HttpClient,
-	routes: Vec<Route>,
-	page: usize,
-	proxies: Option<HashMap<String, Proxy>>,
-	provider: usize,
-	focus: Pane,
-}
-
-impl Default for App {
-	fn default() -> Self {
-		let routes = vec![
-			Route::General,
-			Route::Proxies,
-			Route::Rules,
-			Route::Connections,
-			Route::Logs,
-		];
-
-		Self {
-			http: HttpClient::new(),
-			routes,
-			page: 0,
-			proxies: None,
-			provider: 0,
-			focus: Pane::Menu,
-		}
-	}
-}
-
-impl App {
-	fn navigate(&mut self, page: usize) {
-		self.page = page % self.routes.len();
-		self.fetch_data();
-	}
-
-	fn next_menu(&mut self) {
-		let page = self.page + 1;
-		self.page = page % self.routes.len();
-		self.fetch_data();
-	}
-
-	fn previous_menu(&mut self) {
-		let page = self.page + self.routes.len() - 1;
-		self.page = page % self.routes.len();
-		self.fetch_data();
-	}
-
-	fn fetch_data(&mut self) {
-		if let Some(route) = self.route() {
-			match route {
-				Route::General => {}
-				Route::Proxies => {
-					self.proxies = self.http.proxies().ok()
-				}
-				Route::Rules => {}
-				Route::Connections => {}
-				Route::Logs => {}
-			}
-		}
-	}
-
-	fn route(&self) -> Option<&Route> {
-		self.routes.get(self.page)
-	}
-}
 
 #[derive(PartialEq, Debug)]
 enum Route {
@@ -112,6 +46,9 @@ enum Pane {
 	Proxies,
 	// Other,
 }
+
+const FRAGMENT: &AsciiSet =
+	&CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`');
 
 struct HttpClient {
 	// TODO: async
@@ -144,6 +81,25 @@ impl HttpClient {
 			.json()?;
 		Ok(res.proxies)
 	}
+
+	fn update_proxy(
+		&self,
+		provider: &str,
+		name: &str,
+	) -> Result<(), Box<dyn Error>> {
+		let body = HashMap::from([("name", name)]);
+		self.client
+			.put(format!(
+				"{}{}{}",
+				self.url,
+				"/proxies/",
+				utf8_percent_encode(provider, FRAGMENT),
+			))
+			.json(&body)
+			.send()?
+			.json()?;
+		Ok(())
+	}
 }
 
 #[derive(Deserialize)]
@@ -164,8 +120,219 @@ impl Proxy {
 	}
 }
 
+#[derive(Default)]
+struct ProxiesState {
+	proxies: Option<HashMap<String, Proxy>>,
+	provider: usize,
+	proxy_index: usize,
+	proxies_len: usize,
+	providers_len: usize,
+}
+
+impl ProxiesState {
+	fn fetch_data(&mut self, http: &HttpClient) {
+		self.proxies = http.proxies().ok();
+		if self.proxies.is_none() {
+			self.provider = 0;
+			self.proxy_index = 0;
+			self.providers_len = 0;
+			self.proxies_len = 0;
+		} else {
+			self.provider = 0;
+
+			let providers = self.providers();
+			let len = providers.len();
+
+			if self.providers_len != 0 {
+				self.proxies_len = providers[self.provider]
+					.all
+					.as_ref()
+					.map(|v| v.len())
+					.unwrap_or_default();
+			} else {
+				self.proxies_len = 0;
+			}
+			self.proxy_index = 0;
+
+			self.providers_len = len;
+		}
+	}
+
+	fn providers(&self) -> Vec<&Proxy> {
+		let mut providers = if let Some(proxies) = &self.proxies {
+			proxies.values().filter(|p| p.is_provider()).collect()
+		} else {
+			Vec::new()
+		};
+
+		providers.sort_by(|x, y| x.name.cmp(&y.name));
+
+		providers
+	}
+
+	fn next_tab(&mut self) {
+		if self.providers_len == 0 {
+			self.provider = 0;
+			return;
+		}
+		let index = self.provider + 1;
+		self.provider = index % self.providers_len;
+		let providers = self.providers();
+		self.proxies_len = providers[self.provider]
+			.all
+			.as_ref()
+			.map(|v| v.len())
+			.unwrap_or_default();
+		self.proxy_index = 0;
+	}
+
+	fn previous_tab(&mut self) {
+		if self.providers_len == 0 {
+			self.provider = 0;
+			return;
+		}
+		let index = self.provider + self.providers_len - 1;
+		self.provider = index % self.providers_len;
+		let providers = self.providers();
+		self.proxies_len = providers[self.provider]
+			.all
+			.as_ref()
+			.map(|v| v.len())
+			.unwrap_or_default();
+		self.proxy_index = 0;
+	}
+
+	fn next_proxy(&mut self) {
+		if self.proxies_len == 0 {
+			self.proxy_index = 0;
+			return;
+		}
+		let index = self.proxy_index + 1;
+		self.proxy_index = index % self.proxies_len;
+	}
+
+	fn previous_proxy(&mut self) {
+		if self.proxies_len == 0 {
+			self.proxy_index = 0;
+			return;
+		}
+		let index = self.proxy_index + self.proxies_len - 1;
+		self.proxy_index = index % self.proxies_len;
+	}
+
+	fn select_proxy(&mut self, http: &HttpClient) {
+		if self.providers_len == 0 || self.proxies_len == 0 {
+			return;
+		}
+
+		let providers = self.providers();
+		let provider_index = self.provider;
+		let provider = match providers.get(provider_index) {
+			Some(provider) => provider,
+			_ => return,
+		};
+		let proxy_index = self.proxy_index;
+		let name = match &provider.all {
+			Some(proxies) => {
+				let mut proxies: Vec<_> = proxies
+					.iter()
+					.map(|s| &**s)
+					.collect();
+				proxies.sort();
+
+				match proxies.get(proxy_index) {
+					Some(proxy) => *proxy,
+					_ => return,
+				}
+			}
+			_ => return,
+		};
+
+		http.update_proxy(&provider.name, name).ok();
+		self.fetch_data(http);
+
+		if self.providers_len == 0 || self.proxies_len == 0 {
+			return;
+		}
+		if provider_index < self.providers_len {
+			self.provider = provider_index;
+		}
+		if proxy_index < self.proxies_len {
+			self.proxy_index = proxy_index;
+		}
+	}
+}
+
+struct App {
+	http: HttpClient,
+	routes: Vec<Route>,
+	page: usize,
+	focus: Pane,
+	proxies_state: ProxiesState,
+}
+
+impl Default for App {
+	fn default() -> Self {
+		let routes = vec![
+			Route::General,
+			Route::Proxies,
+			Route::Rules,
+			Route::Connections,
+			Route::Logs,
+		];
+
+		Self {
+			http: HttpClient::new(),
+			routes,
+			page: 0,
+			focus: Pane::Menu,
+			proxies_state: ProxiesState::default(),
+		}
+	}
+}
+
+impl App {
+	fn navigate(&mut self, page: usize) {
+		self.page = page % self.routes.len();
+		self.fetch_data();
+	}
+
+	fn next_menu(&mut self) {
+		let page = self.page + 1;
+		self.page = page % self.routes.len();
+		self.fetch_data();
+	}
+
+	fn previous_menu(&mut self) {
+		let page = self.page + self.routes.len() - 1;
+		self.page = page % self.routes.len();
+		self.fetch_data();
+	}
+
+	fn fetch_data(&mut self) {
+		let route = match self.route() {
+			Some(route) => route,
+			_ => return,
+		};
+		match route {
+			Route::General => {}
+			Route::Proxies => {
+				self.proxies_state.fetch_data(&self.http)
+			}
+			Route::Rules => {}
+			Route::Connections => {}
+			Route::Logs => {}
+		}
+	}
+
+	fn route(&self) -> Option<&Route> {
+		self.routes.get(self.page)
+	}
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-	// TODO: logging
+	// TODO: log
+	// TODO: process arguments
 
 	enable_raw_mode()?;
 	let mut stdout = io::stdout();
@@ -173,7 +340,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 	let backend = CrosstermBackend::new(stdout);
 	let mut terminal = Terminal::new(backend)?;
 
-	let tick_rate = Duration::from_millis(250);
+	let tick_rate = Duration::from_secs(1);
 	let app = App::default();
 	let res = run_app(&mut terminal, app, tick_rate);
 
@@ -233,9 +400,10 @@ fn process_key(code: KeyCode, app: &mut App) -> ProcessResult {
 		Pane::Menu => match code {
 			KeyCode::Char('j') => app.next_menu(),
 			KeyCode::Char('k') => app.previous_menu(),
-			KeyCode::Enter | KeyCode::Char('l') => {
+			KeyCode::Char('l') => {
 				if app.route() == Some(&Route::Proxies) {
 					app.focus = Pane::Proxies;
+					app.fetch_data();
 				}
 			}
 			KeyCode::Char('1') => app.navigate(0),
@@ -249,13 +417,20 @@ fn process_key(code: KeyCode, app: &mut App) -> ProcessResult {
 			KeyCode::Esc | KeyCode::Char('h') => {
 				app.focus = Pane::Menu;
 			}
-			KeyCode::Char('j') => {}
-			KeyCode::Char('k') => {}
+			KeyCode::Char(' ') => {
+				app.proxies_state.select_proxy(&app.http);
+			}
+			KeyCode::Char('j') => {
+				app.proxies_state.next_proxy();
+			}
+			KeyCode::Char('k') => {
+				app.proxies_state.previous_proxy();
+			}
 			KeyCode::Char('H') => {
-				app.provider -= 1;
+				app.proxies_state.previous_tab();
 			}
 			KeyCode::Char('L') => {
-				app.provider += 1;
+				app.proxies_state.next_tab();
 			}
 			_ => {}
 		},
@@ -287,11 +462,10 @@ fn render<B: Backend>(f: &mut Frame<B>, app: &mut App) {
 	let menu = draw_menu(items, page);
 	f.render_widget(menu, chunks[0]);
 
-	let route = &app.route().unwrap_or(&Route::General);
-	let proxies = &app.proxies;
-	let provider = app.provider;
+	let route = &app.routes.get(app.page).unwrap_or(&Route::General);
+	let proxies_state = &mut app.proxies_state;
 	let focus = &app.focus;
-	render_main(f, route, proxies, provider, focus, chunks[1]);
+	render_main(f, route, proxies_state, focus, chunks[1]);
 }
 
 fn draw_menu(items: &[Route], page: usize) -> List<'_> {
@@ -326,16 +500,13 @@ fn draw_menu(items: &[Route], page: usize) -> List<'_> {
 fn render_main<'a, B: Backend>(
 	f: &'a mut Frame<B>,
 	route: &'a Route,
-	proxies: &'a Option<HashMap<String, Proxy>>,
-	provider: usize,
+	state: &mut ProxiesState,
 	focus: &'a Pane,
 	rect: Rect,
 ) {
 	match route {
 		Route::General => f.render_widget(draw_general(), rect),
-		Route::Proxies => {
-			render_proxies(f, proxies, provider, focus, rect)
-		}
+		Route::Proxies => render_proxies(f, state, focus, rect),
 		Route::Rules => f.render_widget(draw_rules(), rect),
 		Route::Connections => f.render_widget(draw_connections(), rect),
 		Route::Logs => f.render_widget(draw_logs(), rect),
@@ -348,8 +519,7 @@ fn draw_general<'a>() -> Block<'a> {
 
 fn render_proxies<'a, B: Backend>(
 	f: &'a mut Frame<B>,
-	proxies: &'a Option<HashMap<String, Proxy>>,
-	provider: usize,
+	state: &mut ProxiesState,
 	focus: &'a Pane,
 	rect: Rect,
 ) {
@@ -363,30 +533,24 @@ fn render_proxies<'a, B: Backend>(
 	let block = Block::default().style(Style::default());
 	f.render_widget(block, rect);
 
-	let mut providers = if let Some(proxies) = proxies {
-		proxies.values().filter(|p| p.is_provider()).collect()
-	} else {
-		Vec::new()
-	};
-	providers.sort_by(|x, y| x.name.cmp(&y.name));
+	if state.providers_len == 0 {
+		return;
+	}
+
+	let providers = state.providers();
 
 	let titles: Vec<_> = providers
 		.iter()
+		.skip(state.provider)
 		.map(|p| Spans::from(p.name.as_ref()))
 		.collect();
-	let len = titles.len();
 
 	let mut tabs = Tabs::new(titles)
 		.block(Block::default().borders(Borders::ALL).title("Proxies"))
 		.style(Style::default())
 		.highlight_style(Style::default().add_modifier(Modifier::BOLD));
 
-	if len == 0 {
-		return;
-	}
-
-	let index = provider % len;
-	tabs = tabs.select(index);
+	tabs = tabs.select(0);
 	if focus == &Pane::Proxies {
 		tabs = tabs.highlight_style(
 			Style::default()
@@ -396,7 +560,8 @@ fn render_proxies<'a, B: Backend>(
 	}
 
 	f.render_widget(tabs, chunks[0]);
-	let provider = providers[index];
+
+	let provider = providers[state.provider];
 	let mut titles: Vec<_> = provider
 		.all
 		.as_ref()
@@ -406,15 +571,19 @@ fn render_proxies<'a, B: Backend>(
 	titles.sort();
 	let items: Vec<_> = titles
 		.iter()
-		.map(|t| {
-			let style = if Some(*t) == provider.now.as_deref() {
-				Style::default()
-					.bg(Color::LightBlue)
-					.add_modifier(Modifier::BOLD)
-			} else {
-				Style::default()
-			};
-			ListItem::new(Spans::from(*t)).style(style)
+		.skip(state.proxy_index)
+		.enumerate()
+		.map(|(i, &t)| {
+			let mut style = Style::default();
+			if Some(t) == provider.now.as_deref() {
+				style = style
+					.fg(Color::LightRed)
+					.add_modifier(Modifier::BOLD);
+			}
+			if i == 0 {
+				style = style.bg(Color::LightBlue);
+			}
+			ListItem::new(Spans::from(t)).style(style)
 		})
 		.collect();
 
